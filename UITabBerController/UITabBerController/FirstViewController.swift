8ca131d27244338655749cd6588d826e53f1db37
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-04-01 22:12:50 +0900

    ThirdViewControllerの追加

---
+++ UITabBerController/UITabBerController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UITabBerController
+//
+//  Created by home on 2018/03/31.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UITabBerController/UITabBerController/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UITabBerController
+//
+//  Created by home on 2018/03/31.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
