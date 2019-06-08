5d68ac06eeaa3a0a1ee5557d72fe5d95fe1e3459
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-06-08 19:47:24 +0900

    CollectionViewのヘッダーをコードからxibで生成するよう変更

--- UITabBerController/UITabBerController/CollectionViewHeader.swift
+++ UITabBerController/UITabBerController/CollectionViewHeader.swift
@@ -2,40 +2,19 @@
 //  CollectionViewHeader.swift
 //  UITabBerController
 //
-//  Created by home on 2019/06/02.
+//  Created by home on 2019/06/08.
 //  Copyright © 2019 Swift-beginners. All rights reserved.
 //
 
 import UIKit
 
-// CollectionViewのヘッダー設定
 class CollectionViewHeader: UICollectionReusableView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setUpViews()
-    }
+
+    @IBOutlet weak var titleLabel: UILabel!
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    var titleLabel: UILabel =  {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 16, weight: UIFont.Weight(rawValue: 10))
-        label.textColor = UIColor.darkGray
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    func setUpViews() {
-        backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)
-        
-        addSubview(titleLabel)
-        
-        titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
-        titleLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1).isActive = true
-        titleLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
     
     func setUpContents(titleText: String) {
