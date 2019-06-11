72a108282334d05bfe5e8a79969d10b4f73a5940
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-06-11 22:43:41 +0900

    UITableViewのサンプル修正

--- UITabBerController/UITabBerController/SecondViewController.swift
+++ UITabBerController/UITabBerController/SecondViewController.swift
@@ -9,11 +9,8 @@
 import UIKit
 
 // テーブルビューに表示するデータ
-let sectionTitle = ["チョウ目", "バッタ目", "コウチュウ目"]
-let section0 = [("キタテハ", "タテハチョウ科"), ("クロアゲハ", "アゲハチョウ科")]
-let section1 = [("キリギリス", "キリギリス科"), ("ヒナバッタ", "バッタ科"), ("マツムシ", "マツムシ科")]
-let section2 = [("ハンミョウ", "ハンミョウ科"), ("アオオサムシ", "オサムシ科"), ("チビクワガタ", "クワガタムシ科")]
-let tableData = [section0, section1, section2]
+let sectionTitle = ["Section1"]
+let section0 = ["item1", "item2", "item3", "item4"]
 
 class SecondViewController: UIViewController {
     
@@ -23,7 +20,6 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        self.tableView.allowsSelection = false
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,35 +28,32 @@ class SecondViewController: UIViewController {
 }
 
 extension SecondViewController: UITableViewDelegate {
+    // セル選択時の処理
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let title = sectionTitle[indexPath.section]
-        let sectionData = tableData[indexPath.section]
-        let cellData = sectionData[indexPath.row]
-        print("\(title)\(cellData.1)")
-        print("\(cellData.0)")
+        print(section0[indexPath.row])
     }
 }
 
 extension SecondViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionData = tableData[section]
-        return sectionData.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitle[section]
-    }
-    
+    // セクション数
     func numberOfSections(in tableView: UITableView) -> Int {
         return sectionTitle.count
     }
     
+    // 各セクションのタイトル
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitle[section]
+    }
+    
+    // セル設定
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
-        let sectionData = tableData[(indexPath as NSIndexPath).section]
-        let cellData = sectionData[(indexPath as NSIndexPath).row]
-        cell.textLabel?.text = cellData.0
-        cell.detailTextLabel?.text = cellData.1
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = section0[indexPath.row]
         return cell
+    }
+    
+    // セルの行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section0.count
     }
 }
