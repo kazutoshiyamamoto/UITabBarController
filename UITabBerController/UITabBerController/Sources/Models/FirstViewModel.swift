0ff1c981ec0058dfa961f1793d27bf8b8276c976
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-06-26 21:35:15 +0900

    FirstView,SecondView用のModelファイル追加

---
+++ UITabBerController/UITabBerController/Sources/Models/FirstViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FirstViewModel.swift
+//  UITabBerController
+//
+//  Created by home on 2019/06/26.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import Foundation
---
+++ UITabBerController/UITabBerController/Sources/Models/SecondViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewModel.swift
+//  UITabBerController
+//
+//  Created by home on 2019/06/26.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import Foundation
