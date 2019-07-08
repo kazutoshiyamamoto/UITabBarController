1bc14292b44a2abb6226bb16ea89e3e7bdeef5f9
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-08 23:56:01 +0900

    FirstViewのスクロール部分に関わるModelクラスを追加

---
+++ UITabBerController/UITabBerController/Sources/Models/Banner.swift
@@ -0,0 +1,16 @@
+//
+//  Banner.swift
+//  UITabBerController
+//
+//  Created by home on 2019/07/08.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewModel: NSObject {
+
+    
+    
+}
+
