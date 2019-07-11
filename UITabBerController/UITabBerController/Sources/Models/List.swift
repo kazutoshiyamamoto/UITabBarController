0395be151938a5263d1497f24e42bc24279689ad
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-11 22:08:16 +0900

    リスト取得のメソッドをListクラスに実装し直す

--- UITabBerController/UITabBerController/Sources/Models/List.swift
+++ UITabBerController/UITabBerController/Sources/Models/List.swift
@@ -16,6 +16,27 @@ class List: NSObject {
     // テーブルビューに表示するデータ
     let sectionTitle = ["Section1"]
     var items: [Item] = []
+    
+    func getTableItems(completionHandler: @escaping ([Item]) -> ()) {
+        let configuration = URLSessionConfiguration.default
+        
+        if let url = URL(string: "http://localhost/test2.php") {
+            WebApi ().getAddConfiguration(url: url, configuration: configuration, completionHandler: {(data, response, error) -> Void in
+                if let data = data {
+                    
+                    do {
+                        let decoder = JSONDecoder()
+                        let items = try decoder.decode([Item].self, from: data)
+                        completionHandler(items)
+                    } catch {
+                        print("Serialize Error")
+                    }
+                } else {
+                    print(error ?? "Error")
+                }
+            })
+        }
+    }
 }
 
 extension List: UITableViewDataSource {
