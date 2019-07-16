4754d8f5724dedf126353b0df0ca7a079529b604
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-16 22:06:18 +0900

    Serviceクラスの追加

---
+++ UITabBerController/UITabBerController/Model/Network/API/Service.swift
@@ -0,0 +1,13 @@
+//
+//  Service.swift
+//  UITabBerController
+//
+//  Created by home on 2019/07/16.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class Service: NSObject {
+    
+}
