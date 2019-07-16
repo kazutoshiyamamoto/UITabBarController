1fda9bf3515e3ad2cf290b461049376bf4022509
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-16 21:47:15 +0900

    画像取得処理を実装するクラスを追加

---
+++ UITabBerController/UITabBerController/Model/Network/API/ImageDownload.swift
@@ -0,0 +1,39 @@
+//
+//  ImageDownload.swift
+//  UITabBerController
+//
+//  Created by home on 2019/07/16.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ImageDownload: NSObject {
+    func loadButtonImage(url: String, completionHandler: @escaping (_ image: UIImage) -> ()) {
+        let configuration = URLSessionConfiguration.default
+        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        configuration.urlCache = nil
+        
+        if let url = URL(string: url) {
+            WebApi().getAddConfiguration(url: url, configuration: configuration, completionHandler: {(data, response, error) -> Void in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                guard let data = data, let response = response as? HTTPURLResponse else {
+                    return
+                }
+                
+                if response.statusCode == 200 {
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)!
+                        completionHandler(image)
+                    }
+                } else {
+                    print(response.statusCode)
+                }
+            }
+            )}
+    }
+}
