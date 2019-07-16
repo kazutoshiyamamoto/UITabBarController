92c489645ebdea3875ca84154333a71d516bf771
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-16 21:57:04 +0900

    不要な実装の削除

--- UITabBerController/UITabBerController/Model/Banner.swift
+++ UITabBerController/UITabBerController/Model/Banner.swift
@@ -16,33 +16,5 @@ class Banner: NSObject {
     let firstButtonImageUrl = "https://cdn-ak.f.st-hatena.com/images/fotolife/h/hfoasi8fje3/20190608/20190608220300.jpg"
     let secondButtonImageUrl = "https://cdn-ak.f.st-hatena.com/images/fotolife/h/hfoasi8fje3/20190608/20190608220253.jpg"
     let thirdButtonImageUrl = "https://cdn-ak.f.st-hatena.com/images/fotolife/h/hfoasi8fje3/20190608/20190608220248.jpg"
-    
-    func loadButtonImage(url: String, completionHandler: @escaping (_ image: UIImage) -> ()) {
-        let configuration = URLSessionConfiguration.default
-        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
-        configuration.urlCache = nil
-        
-        if let url = URL(string: url) {
-            WebApi().getAddConfiguration(url: url, configuration: configuration, completionHandler: {(data, response, error) -> Void in
-                if let error = error {
-                    print(error.localizedDescription)
-                    return
-                }
-                
-                guard let data = data, let response = response as? HTTPURLResponse else {
-                    return
-                }
-                
-                if response.statusCode == 200 {
-                    DispatchQueue.main.async {
-                        let image = UIImage(data: data)!
-                        completionHandler(image)
-                    }
-                } else {
-                    print(response.statusCode)
-                }
-            }
-            )}
-    }
 }
 
