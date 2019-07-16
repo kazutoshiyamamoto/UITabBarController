85c8a0389c5e069928e61484b209ec9a2ce5e08b
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-16 22:12:59 +0900

    ApiClientの追加

---
+++ UITabBerController/UITabBerController/Model/Network/API/ApiClient.swift
@@ -0,0 +1,14 @@
+//
+//  ApiClient.swift
+//  UITabBerController
+//
+//  Created by home on 2019/07/16.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+// ステータスコードなどを実装するクラス
+class ApiClient: NSObject {
+    
+}
