534964f45021e582579f50152e504d3740353759
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-24 23:52:56 +0900

    カスタムクラスの追加

---
+++ UITabBerController/UITabBerController/View/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  UITabBerController
+//
+//  Created by home on 2019/07/24.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
