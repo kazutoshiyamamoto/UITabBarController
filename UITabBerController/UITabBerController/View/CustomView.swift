886276ea1f69d0338317ce7237b1f28ec24a9688
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-24 23:58:19 +0900

    カスタムクラスの実装

--- UITabBerController/UITabBerController/View/CustomView.swift
+++ UITabBerController/UITabBerController/View/CustomView.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class CustomView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
     }
-    */
-
+    
+    func loadNib() {
+        let view = Bundle.main.loadNibNamed("CustomView", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
 }
