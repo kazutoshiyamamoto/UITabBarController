8f3e99fbb4e93e3f882843041e714781c2df820f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-08-10 23:59:10 +0900

    バナー用スクロールビューに高さを設定

---
+++ UITabBerController/UITabBerController/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  constant.swift
+//  UITabBerController
+//
+//  Created by home on 2019/08/03.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+class Constant {
+    let firstButtonImageUrl = "https://cdn-ak.f.st-hatena.com/images/fotolife/h/hfoasi8fje3/20190608/20190608220300.jpg"
+    let secondButtonImageUrl = "https://cdn-ak.f.st-hatena.com/images/fotolife/h/hfoasi8fje3/20190608/20190608220253.jpg"
+    let thirdButtonImageUrl = "https://cdn-ak.f.st-hatena.com/images/fotolife/h/hfoasi8fje3/20190608/20190608220248.jpg"
+    
+    
+}
